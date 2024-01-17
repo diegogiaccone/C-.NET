3bb3d534f384088e6a00291f8d08d72cfc038cea
diegoG <diego_fg91@hotmail.com>
2024-01-17 15:13:36 -0300

    first commit

---
+++ Diego+Giaccone/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diego_Giaccone
+{
+    internal class Usuario
+    {
+    }
+}
