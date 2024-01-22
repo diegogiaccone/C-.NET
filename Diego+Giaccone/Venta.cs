8f594f7a6dac2d02a0c54a084894d1e6451e497c
Diego Giaccone <diego_fg91@hotmail.com>
2024-01-22 14:52:44 -0300

    Update Venta.cs

--- Diego+Giaccone/Venta.cs
+++ Diego+Giaccone/Venta.cs
@@ -8,8 +8,35 @@ namespace Diego_Giaccone
 {
     public class Venta
     {
-        public int _id { get; set; }
-        public string _comentarios { get; set; }
-        public int _idUsuario { get; set; }
+        private int _id;
+        private string _comentarios;
+        private int _idUsuario;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Comentarios
+        {
+            get { return _comentarios; }
+            set { _comentarios = value; }
+        }
+
+        public int IdUsuario
+        {
+            get { return _idUsuario; }
+            set { _idUsuario = value; }
+        }
+
+        // Método para imprimir un recibo de venta
+        public void ImprimirRecibo()
+        {
+            Console.WriteLine($"Recibo de Venta #{_id}");
+            Console.WriteLine($"Comentarios: {_comentarios}");
+            Console.WriteLine($"ID del Usuario: {_idUsuario}");
+            // Agregar más detalles según sea necesario
+        }
     }
 }
