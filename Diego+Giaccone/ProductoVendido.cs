7aa64f95cb39f5ed706f8d420f2427244508e59c
Diego Giaccone <diego_fg91@hotmail.com>
2024-01-22 14:52:17 -0300

    Update ProductoVendido.cs

--- Diego+Giaccone/ProductoVendido.cs
+++ Diego+Giaccone/ProductoVendido.cs
@@ -8,9 +8,39 @@ namespace Diego_Giaccone
 {
     public class ProductoVendido
     {
-        public int _id { get; set; }
-        public int _idProducto { get; set; }
-        public int _cantidadVendida { get; set; }
-        public int _idVenta { get; set; }
+        private int _id;
+        private int _idProducto;
+        private int _cantidadVendida;
+        private int _idVenta;
+  
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public int IdProducto
+        {
+            get { return _idProducto; }
+            set { _idProducto = value; }
+        }
+
+        public int CantidadVendida
+        {
+            get { return _cantidadVendida; }
+            set { _cantidadVendida = value; }
+        }
+
+        public int IdVenta
+        {
+            get { return _idVenta; }
+            set { _idVenta = value; }
+        }
+
+        // Método para calcular el precio total de la venta de este producto
+        public decimal CalcularPrecioTotal(decimal precioUnitario)
+        {
+            return _cantidadVendida * precioUnitario;
+        }
     }
 }
