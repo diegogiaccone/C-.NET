45764ced17b1fa9eae1e7208738c9ac77ce436d8
Diego Giaccone <diego_fg91@hotmail.com>
2024-02-12 01:14:24 -0300

    Update ProductoVendido.cs

--- Diego+Giaccone/ProductoVendido.cs
+++ Diego+Giaccone/ProductoVendido.cs
@@ -3,44 +3,138 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Diego_Giaccone
 {
-    public class ProductoVendido
+    public static class ProductoVendidoData
     {
-        private int _id;
-        private int _idProducto;
-        private int _cantidadVendida;
-        private int _idVenta;
-  
-        public int Id
+        private static string connectionString = "";
+
+        public static ProductoVendido ObtenerProductoVendido(int idProductoVendido)
         {
-            get { return _id; }
-            set { _id = value; }
+            // Implementar lógica para obtener un producto vendido por su ID
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM ProductosVendidos WHERE Id = @Id";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", idProductoVendido);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            // Crear un objeto ProductoVendido con los datos del lector
+                            ProductoVendido productoVendido = new ProductoVendido
+                            {
+                                Id = (int)reader["Id"],
+                                IdVenta = (int)reader["IdVenta"],
+                                IdProducto = (int)reader["IdProducto"],
+                                Cantidad = (int)reader["Cantidad"],
+                                PrecioVenta = (decimal)reader["PrecioVenta"]
+                            };
+                            return productoVendido;
+                        }
+                    }
+                }
+            }
+            return null; // Retornar null si no se encuentra el producto vendido
         }
 
-        public int IdProducto
+        public static List<ProductoVendido> ListarProductosVendidos()
         {
-            get { return _idProducto; }
-            set { _idProducto = value; }
+            // Implementar lógica para listar todos los productos vendidos
+            List<ProductoVendido> productosVendidos = new List<ProductoVendido>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM ProductosVendidos";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Crear objetos ProductoVendido y agregar a la lista
+                            ProductoVendido productoVendido = new ProductoVendido
+                            {
+                                Id = (int)reader["Id"],
+                                IdVenta = (int)reader["IdVenta"],
+                                IdProducto = (int)reader["IdProducto"],
+                                Cantidad = (int)reader["Cantidad"],
+                                PrecioVenta = (decimal)reader["PrecioVenta"]
+                            };
+                            productosVendidos.Add(productoVendido);
+                        }
+                    }
+                }
+            }
+            return productosVendidos;
         }
 
-        public int CantidadVendida
+        public static void CrearProductoVendido(ProductoVendido productoVendido)
         {
-            get { return _cantidadVendida; }
-            set { _cantidadVendida = value; }
+            // Implementar lógica para crear un nuevo producto vendido
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO ProductosVendidos (IdVenta, IdProducto, Cantidad, PrecioVenta) " +
+                               "VALUES (@IdVenta, @IdProducto, @Cantidad, @PrecioVenta)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@IdVenta", productoVendido.IdVenta);
+                    command.Parameters.AddWithValue("@IdProducto", productoVendido.IdProducto);
+                    command.Parameters.AddWithValue("@Cantidad", productoVendido.Cantidad);
+                    command.Parameters.AddWithValue("@PrecioVenta", productoVendido.PrecioVenta);
+
+                    command.ExecuteNonQuery();
+                }
+            }
         }
 
-        public int IdVenta
+        public static void ModificarProductoVendido(ProductoVendido productoVendido)
         {
-            get { return _idVenta; }
-            set { _idVenta = value; }
+            // Implementar lógica para modificar un producto vendido existente
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "UPDATE ProductosVendidos SET IdVenta = @IdVenta, IdProducto = @IdProducto, " +
+                               "Cantidad = @Cantidad, PrecioVenta = @PrecioVenta WHERE Id = @Id";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", productoVendido.Id);
+                    command.Parameters.AddWithValue("@IdVenta", productoVendido.IdVenta);
+                    command.Parameters.AddWithValue("@IdProducto", productoVendido.IdProducto);
+                    command.Parameters.AddWithValue("@Cantidad", productoVendido.Cantidad);
+                    command.Parameters.AddWithValue("@PrecioVenta", productoVendido.PrecioVenta);
+
+                    command.ExecuteNonQuery();
+                }
+            }
         }
 
-        // Método para calcular el precio total de la venta de este producto
-        public decimal CalcularPrecioTotal(decimal precioUnitario)
+        public static void EliminarProductoVendido(int idProductoVendido)
         {
-            return _cantidadVendida * precioUnitario;
+            // Implementar lógica para eliminar un producto vendido por su ID
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "DELETE FROM ProductosVendidos WHERE Id = @Id";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", idProductoVendido);
+
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
